ec83fab41b9613b84d126fdb9f0353cb4da4d123
Arijit Das [Itobuz] <arijit@itobuz.com>
2025-05-20 19:49:33 +0530

    init

--- 05_output.sh
+++ 05_output.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+# Creates a new file or overwrites an existing file
+echo "Hello World 1" > ./tmp/1.txt 
 
-echo "Hello World" > /tmp/1.txt
+# Appends to an existing file
+# The file will be created if it does not exist
+echo "Hello World 2" >> ./tmp/1.txt 
+
+echo "Hello World 2" >> ./tmp/2.txt
